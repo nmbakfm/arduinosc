71fc636e84bc2b5efe6cef44533a80c0d476ce20
南部 晃史 <nmbakfm@inter-arteq.com>
2015-09-23 17:36:03 +0900

    ひとまず完成

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 # A sample Gemfile
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
+gem 'json'
 gem 'arduino_firmata'
 gem 'osc-ruby'
--- arduinosc.rb
+++ arduinosc.rb
@@ -1,28 +1,47 @@
 require 'bundler'
 Bundler.require
 
-json_file_path = 'settings.json'
-
+json_file_path = File.expand_path('./setting.json')
 json_data = open(json_file_path) do |io|
   JSON.load(io)
 end
 
 Host = json_data['osc']['host']
 Port = json_data['osc']['port']
-c = OSC::UDPSocket.new
+c = OSC::Client.new(Host, Port)
 
-available_pins = json_data['arduino']['available_pins']
+arduino_settings = json_data['arduino']
+available_pins = arduino_settings['available_pins']
 
-messages = []
-ArduinoFirmata.connect do
-  available_pins['digital'].each do |pin|
-    messages << OSC::Message.new("/digital/pin#{pin}", digital_read(pin))
-  end
-  available_pins['analog'].each do |pin|
-    messages << OSC::Message.new("/analog/pin#{pin}", analog_read(pin))
-  end
+arduino = ArduinoFirmata.connect arduino_settings['modem']
+puts "firmata version #{arduino.version}"
+
+available_pins['digital'].each do |pin|
+  arduino.pin_mode pin, ArduinoFirmata::OUTPUT
 end
 
-bundle = OSC::Bundle.new(nil, *messages)
+loop do
+  messages = []
 
-c.send(bundle)
+  available_pins['digital'].each do |pin|
+    messages << OSC::Message.new(
+      "/digital/pin#{pin}",
+      arduino.digital_read(pin) ? 1 : 0
+    )
+    print((arduino.digital_read(pin) ? 1 : 0).to_s + ' ')
+  end
+  puts "\n"
+  available_pins['analog'].each do |pin|
+    messages << OSC::Message.new(
+      "/analog/pin#{pin}",
+      arduino.analog_read(pin)
+    )
+    print(arduino.analog_read(pin).to_s + ' ')
+  end
+  puts "\n"
+  puts "\n"
+
+  bundle = OSC::Bundle.new(nil, *messages)
+  c.send(bundle)
+  sleep(0.05)
+end
